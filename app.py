df33f0c6bc53d12ff402fc38eacb8d775fc3f30a
Daniel Soler Fontanet <daniel.soler@students.salle.url.edu>
2024-12-13 09:51:16 +0900

    NeuCF Training correctly

--- app.py
+++ app.py
@@ -1,14 +1,23 @@
-import train as t
+#import train as t
 import numpy as np
-
+import train3 as md
+import dataset as dt
+import torch
 
 class UserInterface:
 
     def __init__(self, current_user = 1):
-        self.user_embeddings = t.user_embeddings
-        self.movie_embeddings = t.movie_embeddings
+        #self.user_embeddings = t.user_embeddings
+        #self.movie_embeddings = t.movie_embeddings
+        self.user = 0
+        self.movies = dt.readMovies()
         self.current_user = current_user
+        self.ratings = dt.readRatings()
+        self.model = md.loadModel(len(self.ratings['userId'].unique()), len(self.ratings['movieId'].unique()), 128)
+        self.item_mapping = {item_id: idx for idx, item_id in enumerate(self.ratings['movieId'].unique())}
+        self.reverse_item_mapping = {idx: item_id for item_id, idx in self.item_mapping.items()}
         self.run_ui()
+
 
     def run_ui(self):
         """Keeps running the UI until the user decides to quit."""
@@ -25,11 +34,11 @@ class UserInterface:
                     self.current_user = new_user
                     print(f"User changed to {self.current_user}")
                     # Perform a forward pass with the new user embedding and a random movie embedding
-                    user_emb = self.user_embeddings[self.current_user - 1]
-                    movie_idx = np.random.randint(len(self.movie_embeddings))
-                    movie_emb = self.movie_embeddings[movie_idx]
-                    _, _, _, _, _, _, y_pred = t.forward(user_emb, movie_emb)
-                    print(f"Predicted rating for a random movie: {y_pred[0][0]:.2f}")
+                    #user_emb = self.user_embeddings[self.current_user - 1]
+                    #movie_idx = np.random.randint(len(self.movie_embeddings))
+                    #movie_emb = self.movie_embeddings[movie_idx]
+                    #_, _, _, _, _, _, y_pred = t.forward(user_emb, movie_emb)
+                    #print(f"Predicted rating for a random movie: {y_pred[0][0]:.2f}")
                     break
                 else:
                     print("Invalid user ID. Please enter a number between 1 and 610.")
@@ -75,17 +84,53 @@ class UserInterface:
         return 1 <= user_input <= 4
 
     def give_rating(self):
+        print("Select which movie yo would like to watch: ( Write the id is showed in the movie dataset :) )")
+        id = int(input())
+        print("Which rating do you want to give it to the movie?")
+        rating = float(input())
+
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        user_id_tensor = torch.tensor([self.current_user]).to(device)
+        idx = self.item_mapping.get(id)
+        item_id_tensor = torch.tensor([idx]).to(device)
+        with torch.no_grad():  # No necessitem gradients per a la predicció
+            y_pred = self.model(user_id_tensor, item_id_tensor).item()  # Obtenim el valor de la predicció
+        print(f"Predicted Rating: {y_pred*5:.2f}")
+        self.model.adapt_user_embedding(user_id_tensor, item_id_tensor, rating)
+
+        similar_movies, scores = self.model.find_similar_movies(idx, 5)
+
+        print(f"\nSimilar movies to {self.movies.loc[self.movies["movieId"] == id, 'title'].iloc[0]} : ")
+        for i, (sim_movie, score) in enumerate(zip(similar_movies, scores)):
+            print(f"{i + 1}. Movie Title:  {self.movies.loc[self.movies["movieId"] == self.reverse_item_mapping.get(sim_movie.item()), "title"].iloc[0]} - Similarity: {score.item():.4f}")
+
+
+
         """Placeholder for the give rating functionality."""
+
         print("Rating Success")
 
     def get_recommendation(self):
         """Recommend top movies for the current user based on their embedding."""
-        user_emb = self.user_embeddings[self.current_user - 1]
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        user_id_tensor = torch.tensor([self.current_user]).to(device)
+
+
+
         predictions = []
 
-        for idx, movie_emb in enumerate(self.movie_embeddings):
-            _, _, _, _, _, _, y_pred = t.forward(user_emb, movie_emb)
-            predictions.append((idx, y_pred[0][0]))
+        for index, movie in self.movies.iterrows():
+            if index == 0:
+                continue
+
+            idx = self.item_mapping.get(movie[0])
+            if idx is None:
+                continue
+            item_id_tensor = torch.tensor([idx]).to(device)
+
+            with torch.no_grad():  # No necessitem gradients per a la predicció
+                y_pred = self.model(user_id_tensor, item_id_tensor).item()  # Obtenim el valor de la predicció
+            predictions.append((movie[0], y_pred * 5))
 
         # Sort movies by predicted rating in descending order
         predictions.sort(key=lambda x: x[1], reverse=True)
@@ -93,7 +138,8 @@ class UserInterface:
         print("\nTop 5 Movie Recommendations:")
         for i in range(5):
             movie_idx, rating = predictions[i]
-            print(f"Movie {movie_idx + 1}: Predicted Rating: {rating:.2f}")
+            peli = self.movies[self.movies["movieId"] == movie_idx]["title"]
+            print(f"Movie: {peli.iloc[0]}: ID: {movie_idx}: Predicted Rating: {rating:.2f}")
 
 
 if __name__ == "__main__":
--- train3.py
+++ train3.py
@@ -4,6 +4,7 @@ from tensorflow.python.ops.ragged.ragged_dispatch import ragged_binary_elementwi
 
 import dataset as ds
 import pandas as pd
+import torch.nn.functional as F
 from torch.utils.data import Dataset, DataLoader
 
 class InteractionDataset(Dataset):
@@ -18,7 +19,7 @@ class InteractionDataset(Dataset):
     def __getitem__(self, idx):
         return self.users[idx], self.items[idx], self.labels[idx]
 
-
+model_path = "model.pth"
 ratings = ds.readRatings()
 movies = ds.readMovies()
 ratings = ratings.join(movies.set_index('movieId'), on='movieId')
@@ -27,7 +28,7 @@ item_mapping = {item_id: idx for idx, item_id in enumerate(ratings['movieId'].un
 
 # Exemple de dataset
 train_dataset = InteractionDataset(ratings["userId"], ratings["movieId"], ratings["rating"], item_mapping)
-train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+train_loader = DataLoader(train_dataset, batch_size=200, shuffle=True)
 
 class NeuCF(nn.Module):
     def __init__(self, num_users, num_items, embedding_dim):
@@ -41,11 +42,17 @@ class NeuCF(nn.Module):
 
         # MLP layers
         self.mlp = nn.Sequential(
-            nn.Linear(embedding_dim * 2, 128),
+            nn.Linear(embedding_dim * 2, 256),  # Més unitats a la primera capa
+            nn.BatchNorm1d(256),  # Normalització de lot
             nn.ReLU(),
-            nn.Linear(128, 64),
+            nn.Linear(256, 128),  # Segona capa augmentada
+            nn.BatchNorm1d(128),  # Normalització de lot
             nn.ReLU(),
-            nn.Linear(64, embedding_dim)
+            nn.Linear(128, 64),  # Tercera capa augmentada
+            nn.BatchNorm1d(64),  # Normalització de lot
+            nn.ReLU(),
+            nn.Linear(64, embedding_dim),  # Capa de sortida
+            nn.Dropout(0.3)  # Dropout per evitar sobreajustament
         )
 
         # Output layer
@@ -56,23 +63,59 @@ class NeuCF(nn.Module):
         user_embed = self.user_embedding(user_ids)
         item_embed = self.item_embedding(item_ids)
 
-        # GMF interaction
         gmf = user_embed * item_embed
-
-        # MLP interaction
         mlp_input = torch.cat([user_embed, item_embed], dim=-1)
         mlp = self.mlp(mlp_input)
 
-        # Combine GMF and MLP
         combined = torch.cat([gmf, mlp], dim=-1)
 
-        # Output layer
         pred = self.sigmoid(self.output(combined))
         return pred
 
-def train(model : NeuCF, criterion, optimizer, device):
-    num_epochs = 50  # Pots ajustar-ho segons el dataset
+    def forwardAdapt(self, user_embed, item_embed):
 
+        gmf = user_embed * item_embed
+
+        mlp_input = torch.cat([user_embed, item_embed], dim=-1)
+        mlp = self.mlp(mlp_input)
+
+        combined = torch.cat([gmf, mlp], dim=-1)
+
+        pred = self.sigmoid(self.output(combined))
+        return pred
+
+    def find_similar_movies(model, movie_id, top_k=5):
+
+        movie_emb = model.item_embedding(torch.tensor(movie_id))
+
+        all_movie_embs = model.item_embedding.weight
+
+        similarity = F.cosine_similarity(movie_emb, all_movie_embs, dim=1)
+
+        top_similar = torch.argsort(similarity, descending=True)[:top_k]
+
+        return top_similar, similarity[top_similar]
+    def adapt_user_embedding(self, user_id, new_movie_ids, new_ratings, lr=0.2, steps=200):
+
+        user_emb = self.user_embedding(user_id).clone().detach().requires_grad_(True)
+        optimizer = torch.optim.SGD([user_emb], lr=lr)
+
+        new_ratings = torch.tensor(new_ratings, dtype=torch.float32)
+
+        # Entrena només sobre les noves valoracions
+        for _ in range(steps):
+            optimizer.zero_grad()
+            movie_embs = self.item_embedding(new_movie_ids).clone().detach().requires_grad_(True)
+
+            predictions = self.forwardAdapt(user_emb, movie_embs)
+            loss = nn.MSELoss()(predictions/5, new_ratings/5)
+            loss.backward()
+            optimizer.step()
+
+        self.user_embedding.weight.data[user_id] = user_emb.data
+
+def train(model : NeuCF, criterion, optimizer, device, scheduler):
+    num_epochs = 100
     for epoch in range(num_epochs):
         model.train()
         total_loss = 0
@@ -80,14 +123,8 @@ def train(model : NeuCF, criterion, optimizer, device):
         for user_ids, item_ids, labels in train_loader:
             # Mou dades a la GPU si tens CUDA disponible
             user_ids, item_ids, labels = user_ids.to(device), item_ids.to(device), labels.to(device)
-
-            # Reseta els gradients
             optimizer.zero_grad()
-
-            # Prediccions
             predictions = model(user_ids, item_ids).squeeze()
-
-            # Calcula la pèrdua
             loss = criterion(predictions, labels.float())
 
             # Backpropagation i optimització
@@ -96,23 +133,44 @@ def train(model : NeuCF, criterion, optimizer, device):
 
             total_loss += loss.item()
 
+        val_loss = total_loss / len(train_loader)
+        scheduler.step(val_loss)
+
         print(f"Epoch {epoch + 1}/{num_epochs}, Loss: {total_loss / len(train_loader):.4f}")
 
+
+
+def init_weights(m):
+    if isinstance(m, nn.Linear):
+        nn.init.xavier_uniform_(m.weight)
+        if m.bias is not None:
+            nn.init.zeros_(m.bias)
+
+def loadModel(num_users, num_items, embedding_dim):
+    model = NeuCF(num_users, num_items, embedding_dim)
+    # Carrega l'estat guardat
+    model.load_state_dict(torch.load(model_path))
+
+    # Passa el model a eval per fer prediccions
+    model.eval()
+    return model
 
 def setTraining():
     global ratings, movies
     num_users = len(ratings['userId'].unique())  # Comptar usuaris únics
     num_items = len(ratings['movieId'].unique())  # Comptar pel·lícules úniques
-
-
-    embedding_dim = 60
+    embedding_dim = 128
 
     model = NeuCF(num_users, num_items, embedding_dim)
-    criterion = nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.015)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-    train(model, criterion, optimizer, device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.002)
+    scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.1)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    model.apply(init_weights)
+
+    train(model, criterion, optimizer, device, scheduler)
+    torch.save(model.state_dict(), model_path)
 
 def getUserEmbbeding():
     movies = ds.readMovies()
@@ -139,4 +197,4 @@ def getUserEmbbeding():
     userPreferencesDF = pd.DataFrame(userPreferences).T
     return userPreferencesDF
 
-setTraining()
+#setTraining()
